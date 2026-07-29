c8d8f98abbecb2549ffd1664aa10af2b503b7559
17726441075 <17726441075@189.cn>
2026-07-29 10:01:03 +0800

    fisrt

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    /*
+     * Server commands for this project:
+     * ssh -i D:\pzl\pzl.pem root@47.91.31.90
+     * scp upload:   scp -i D:\pzl\pzl.pem target/order-1.0.jar root@47.91.31.90:~
+     * scp download: scp -i D:\pzl\pzl.pem root@47.91.31.90:~/order-1.0.jar target/order-1.0.jar
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
